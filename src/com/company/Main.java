3cfcd2965d53aeaa4acdd0ef278ec922545cf0d2
roukorjerte <roukorjerte@gmail.com>
2021-07-05 19:03:49 +0300

    learned about method types and how to return them

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
         Character paladin = new Character();
-        paladin.name = "Gilbert";
+        paladin.setNameAndAge("Gilbert", 25);
         paladin.className = "Paladin";
-        paladin.age = 25;
+        paladin.mana = 100;
         paladin.powers = new String[]{"Saint fist", "Power of God", "Stone heart"};
 
         paladin.speak();
         paladin.spells();
 
+        int newMana1 = paladin.calculateMana();
+        System.out.println("Now " + paladin.name + " mana is " + paladin.mana
+                + " and after using " +paladin.powers[0] +" it will be " + newMana1);
+
+        System.out.println(" ");
+
         Character mage = new Character();
-        mage.name = "Andrew";
+        mage.setNameAndAge("Andrew", 17);
         mage.className = "Mage";
-        mage.age = 17;
+        mage.mana = 300;
         mage.powers = new String[]{"Fire blast", "Lightning strike", "Elemental attack"};
 
         mage.speak();
@@ -28,12 +34,23 @@ class Character{
     String className;
     int age;
     String[] powers;
+    int mana;
 
     void speak(){
-        System.out.println(name +" is " +className +", he is " +age +" years old.");
+        System.out.println(name + " is " + className + ", he is " + age + " years old.");
     }
 
     void spells(){
-        System.out.println(name +" can cast such spells as: " +powers[0] +", " +powers[1] +" and " +powers[2] );
+        System.out.println(name + " can cast such spells as: " + powers[0] +", " + powers[1] + " and " + powers[2] );
+    }
+
+    int calculateMana(){
+        int newMana = mana - 25;
+        return newMana;
+    }
+
+    void setNameAndAge(String username, int years){
+        name = username;
+        age =  years;
     }
 }
